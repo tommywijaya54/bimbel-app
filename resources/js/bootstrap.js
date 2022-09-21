2cc022d12c27df886a1d86251643a03db5c2b9e7
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-21 12:33:47 +1000

    adding capitalisation

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,3 +32,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
 //     enabledTransports: ['ws', 'wss'],
 // });
+
+
+String.prototype.cap = function (){
+        return (this.charAt(0).toUpperCase() + this.slice(1));
+    }
+    
