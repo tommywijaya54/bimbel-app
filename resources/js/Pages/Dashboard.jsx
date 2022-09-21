f0c44bbf7f83fbd7d68c39b043e595c7c0f71605
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-21 12:33:27 +1000

    Update Dashboard.jsx

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -3,11 +3,14 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/inertia-react';
 
 export default function Dashboard(props) {
+    const links = ['user','parent'];
+
     return (
         <AuthenticatedLayout
             auth={props.auth}
             errors={props.errors}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
+            links={links}
         >
             <Head title="Dashboard" />
 
