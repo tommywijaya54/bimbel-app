e3de0291cf57df5d30c3c5ef7c88607b4b57e521
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-23 14:18:38 +1000

    Update SimpleLink.jsx

--- resources/js/Shared/SimpleLink.jsx
+++ resources/js/Shared/SimpleLink.jsx
@@ -4,11 +4,16 @@ import Icon from './Icon';
 
 export default ({type,id,href,children}) => {
     let newhref = href+"/"+id;
-    let classNames = 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+    let classNames = 'link inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
 
     if(type == 'user'){
         classNames += " user";
     }
+
+    if(type == 'goto'){
+        classNames += " goto";
+    }
+
 
     return (<Link
             href={newhref}
