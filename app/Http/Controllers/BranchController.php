844455715e4aa055f54aa50993453e37c4547467
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-25 15:59:39 +1000

    Adding Branch List

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Branch;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class BranchController extends Controller
 {
     //
+
+    public function index()
+    {
+        $data = Branch::all();
+        return Inertia::render('Simple/Index', [
+            'pagetitle' => "Branches List",
+            'data' => $data,
+            'goto' => 'branch',
+            'view' => "id,name,phone,address,email"
+        ]);
+    }
 }
