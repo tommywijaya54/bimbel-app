4d3f96a3308ac46b1fc7920de242527e2185162b
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-25 16:04:29 +1000

    Adding Reset Password Link

--- resources/js/Components/SmartFooterButton.jsx
+++ resources/js/Components/SmartFooterButton.jsx
@@ -13,6 +13,7 @@ const SmartFooterButton = ({componentFor, obj}) => {
             deactivate:'/user/'+obj.id+'/deactivate',
             activate:'/user/'+obj.id+'/activate',
             edit:'/user/'+obj.id+'/edit',
+            reset_password:'/user/'+obj.id+'/resetpassword',
         }
         current_user.hasPermission('deactivate-user') ? 
             component.push(<a href={url.deactivate} className={'button ' + (obj.disabled ? 'disabled' : '')}>Deactivate user</a>) : ''
@@ -20,11 +21,14 @@ const SmartFooterButton = ({componentFor, obj}) => {
         current_user.hasPermission('activate-user') ? 
             component.push(<a href={url.activate} className={'button ' + (obj.disabled ? '' : 'disabled')}>Activate user</a>) : ''
         
+        current_user.hasPermission('reset password-user') ? 
+            component.push(<a href={url.reset_password} className={'button '}>Reset Password</a>) : ''
+
         current_user.hasPermission('edit-user') ? 
             component.push(<a href={url.edit} className={'button '}>Edit user</a>) : ''
 
         
-            /*
+        /*
         current_user.hasPermission('deactivate-user') ? 
             component.push(<button type="button" className="text-red-600 hover:underline">Deactivate User</button>) : ''
         
@@ -33,8 +37,10 @@ const SmartFooterButton = ({componentFor, obj}) => {
         
         current_user.hasPermission('activate-user') ? 
             component.push(<button type="button" className="flex items-center btn-indigo ml-auto" >Activate User</button>) : '' 
-        */ 
+        */  
     }
+
+    
 
     return (<div className="smart-footer">
                 <div className="px-6 py-4 bg-gray-50 flex justify-end">
