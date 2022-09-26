f760ccec4cb3fd6e8354c0f868398720653fba68
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-26 14:04:09 +1000

    Adding Branch Footer Button

--- resources/js/Components/SmartFooterButton.jsx
+++ resources/js/Components/SmartFooterButton.jsx
@@ -5,15 +5,14 @@ const SmartFooterButton = ({componentFor, obj}) => {
         const permissions = data.auth.permissions;
     */
 
-    const component = [];
+    let component = [];
 
-    if(componentFor === 'user'){
-        // edit-user deactivate-user activate-user
+    if(componentFor === 'User'){
         const url = {
             deactivate:'/user/'+obj.id+'/deactivate',
             activate:'/user/'+obj.id+'/activate',
             edit:'/user/'+obj.id+'/edit',
-            reset_password:'/user/'+obj.id+'/resetpassword',
+            resetPassword:'/user/'+obj.id+'/resetpassword',
         }
         current_user.hasPermission('deactivate-user') ? 
             component.push(<a href={url.deactivate} className={'button ' + (obj.disabled ? 'disabled' : '')}>Deactivate user</a>) : ''
@@ -22,25 +21,20 @@ const SmartFooterButton = ({componentFor, obj}) => {
             component.push(<a href={url.activate} className={'button ' + (obj.disabled ? '' : 'disabled')}>Activate user</a>) : ''
         
         current_user.hasPermission('reset password-user') ? 
-            component.push(<a href={url.reset_password} className={'button '}>Reset Password</a>) : ''
+            component.push(<a href={url.resetPassword} className={'button '}>Reset Password</a>) : ''
 
         current_user.hasPermission('edit-user') ? 
             component.push(<a href={url.edit} className={'button '}>Edit user</a>) : ''
-
-        
-        /*
-        current_user.hasPermission('deactivate-user') ? 
-            component.push(<button type="button" className="text-red-600 hover:underline">Deactivate User</button>) : ''
-        
-        current_user.hasPermission('edit-user') ? 
-            component.push(<button type="button" className="flex items-center btn-indigo ml-auto" >Edit User</button>) : ''
-        
-        current_user.hasPermission('activate-user') ? 
-            component.push(<button type="button" className="flex items-center btn-indigo ml-auto" >Activate User</button>) : '' 
-        */  
     }
 
-    
+    if(componentFor === 'Branch'){
+        current_user.hasPermission('delete-branch') ? 
+            component.push(<a href={'/branch/'+obj.id+'/delete'} className={'button '}>Delete Branch</a>) : ''
+
+        current_user.hasPermission('edit-branch') ? 
+            component.push(<a href={'/branch/'+obj.id+'/edit'} className={'button '}>Edit Branch</a>) : ''
+    }
+
 
     return (<div className="smart-footer">
                 <div className="px-6 py-4 bg-gray-50 flex justify-end">
