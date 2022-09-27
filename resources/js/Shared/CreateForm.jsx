662a9c2ce00ad85177abb4fd0292a8740f396fbd
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-27 19:20:50 +1000

    added form fields options to CreateForm & EditForm

--- resources/js/Shared/CreateForm.jsx
+++ resources/js/Shared/CreateForm.jsx
@@ -6,23 +6,32 @@ import FormElement from './FormElement';
 
 // For use in useForm (Inertia/React)  
 class FormObject {
-    constructor(str,data){
-        this.initialString = str;
-        this.Object = str.fromStringArraytoObject();
-        this.DisplayElementInArray = (str.split(',')).map((a) => {
-            return a.toDisplayElement();
-        });
+    constructor(form_fields,data,form_fields_options){
+        this.initialString = form_fields;
 
+        this.Object = (form_fields.replace(",,",',')).fromStringArraytoObject();
         if(data){
             for(const property in this.Object){
-                this.Object[property] = data[property];
+                this.Object[property] = data[property] == null ? '' : data[property];
             }
         }
+
+        this.DisplayElementInArray = (form_fields.split(',')).map((a) => {
+            let DisplayEl = a.toDisplayElement();
+
+            if(form_fields_options && form_fields_options[DisplayEl.entityname]){
+                DisplayEl.label = (DisplayEl.entityname.split("_id")[0]).cap();
+
+                DisplayEl.options = ([{id:'',name:''}]).concat(form_fields_options[DisplayEl.entityname]);
+            }
+
+            return DisplayEl;
+        });
     }
 }
 
 const CreateForm = ({props}) => {
-    const FormOb = new FormObject(props.form_fields);
+    const FormOb = new FormObject(props.form_fields,null,props.form_fields_options);
     const { data, setData, errors, post, processing } = useForm({
         ...FormOb.Object
     });
