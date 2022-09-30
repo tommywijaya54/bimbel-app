da729da4c348665b2f5e4b5c96259e393199bc93
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-09-30 13:20:36 +1000

    Fixing Top Navigation using array.filter

--- resources/js/Components/TopNavigationList.jsx
+++ resources/js/Components/TopNavigationList.jsx
@@ -5,6 +5,20 @@ export default function TopNavigationList() {
     const permissions = usePage().props.auth.permissions;
     
     const NavList = ({list}) => {
+        return list.filter(alist => alist.includes('list-')).map(((listonly,i) => {
+            const model = listonly.split("list-")[1];
+            const href = "/"+model.replaceAll(" ",'-');
+            return <Link
+                        key={i}
+                        href={href}
+                        className = 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+                        >
+                        {model.cap()}
+                    </Link>
+        }))
+
+
+        /*
         return list.map((l,i) => {
             if(l.includes("list")){
                 const href = l.split("list-")[1];
@@ -19,6 +33,7 @@ export default function TopNavigationList() {
             }
             return null;
         })
+        */
     }
 
     return <>
