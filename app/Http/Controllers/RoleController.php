de6346764990c16542d78ee130ec7b4e265e5f13
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-04 13:14:32 +1100

    Refactoring Role: Show & Edit

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -7,6 +7,7 @@ use App\ListSchema;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class RoleController extends CommonController
@@ -18,8 +19,22 @@ class RoleController extends CommonController
     {
         parent::__construct([
             'list' => 'id,name',
-            'form' => 'name'
+            'form' => 'name,permissions'
         ], true);
+
+        $this->form->title_format = '{name}';
+
+        $permissions_field = $this->form->field('permissions');
+        $permissions_field->hasOptions(
+            Permission::pluck('name')->toArray(),
+            'multiple-checkbox'
+        );
+        $permissions_field->special_request = 'group-the-options';
+        $permissions_field->getValue = function ($field, $id) {
+            $role = Role::find($id);
+            $value = $role->permissions()->pluck('name')->toArray();
+            return $value;
+        };
     }
 
     public function create()
@@ -29,33 +44,42 @@ class RoleController extends CommonController
             'form_schema' => $this->form->createForm(),
         ]);
     }
+
     public function show($id)
     {
         $form_data = $this->form->displayForm($id);
-        $role = $this->entity::find($id);
 
-        $role_permission = $role->permissions()->get()->toArray();
+        $role = $this->entity::find($id);
         $users = User::role($role->name)->get()->toArray();
+
+        $role_permissions = $role->permissions()->pluck('name')->toArray();
+        $available_permissions = Permission::pluck('name')->toArray();
 
         return Inertia::render('Role/Show', [
             'title' => $form_data->title,
             'form_schema' => $form_data,
-            'role_permission' => $role_permission,
             'users' => $users,
+            'role_permissions' => $role_permissions,
+            'available_permissions' => $available_permissions,
         ]);
     }
+
     public function edit($id)
     {
         $form_data = $this->form->editForm($id);
-
-        $role = $this->entity::find($id);
-
-        $role_permission = $role->permissions()->get()->toArray();
-        $available_permissions = Permission::select('id', 'name')->get()->toArray();
-
-        return Inertia::render('Common/EditForm', [
+        return Inertia::render('Role/EditForm', [
             'title' => 'Edit ' . $form_data->title . ' ' . $this->modal_name_for_page_title,
-            'form_schema' => $form_data,
+            'form_schema' => $form_data
         ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $role = Role::findById($id);
+        $role->name = $request['name'];
+        $role->update();
+
+        $role->syncPermissions($request['permissions']);
+        return redirect('/' . $this->modal . '/' . $id);
     }
 }
--- resources/js/Pages/Role/Show.jsx
+++ resources/js/Pages/Role/Show.jsx
@@ -3,17 +3,16 @@ import MainLayout from '@/Layouts/MainLayout';
 import Form from '@/Shared/DisplayPageComponent/Form/Form';
 
 export default function Show(props) {
-    const permissionGroupByCategory = props.role_permission.reduce((group, permission) => {
-        const group_name = permission.name.split("-")[1];
-
+    const groupedPermissions = props.available_permissions.reduce((group, permission) => {
+        const group_name = permission.split("-")[1];
         group[group_name] = group[group_name] ?? [];
         group[group_name].push(permission);
-
         return group;
     }, {});
 
-    console.log(permissionGroupByCategory);
-
+    const permissionCheck = (permission_param) => {
+        return props.role_permissions.find(permission => permission === permission_param);
+    }
 
     return (
         <MainLayout
@@ -22,36 +21,40 @@ export default function Show(props) {
             <Form
                 {...props.form_schema}
             >
-                <div className="form-component pr-6 pb-8 w-full lg:w-1/2">
-                    <label className="form-label">Users:</label>
+                <div className="form-component pr-6 pb-8 w-full">
+                    <label className="form-label">People that have this role:</label>
                     <div className="form-input">
-                        {props.users.map(user => <span className="unit">{user.name}</span>)}
+                        {props.users.length == 0 && <span className='empty-value'>No one assign to this role</span>}
+                        {props.users.map((user, keyid) => {
+                            return <span key={keyid} className="unit">{user.name}</span>
+                        })}
                     </div>
                 </div>
-
-                <div>
+                        
+                <div className='pt-8'>
                     <div className="form-component pr-6 pb-8 w-full">
-                        <label className="form-label">Role Permissions:</label>
+                        <label className="form-label">Permissions that this role have:</label>
                         <div className="form-input">
                             <table>
-                                {Object.keys(permissionGroupByCategory).map((propertyKey, keyid)=>{
+                                <tbody>
+                                {Object.keys(groupedPermissions).map((propertyKey, keyid)=>{
                                     return (
-                                        <tr key={keyid}>
-                                            <td><strong className='pr-6'>{propertyKey.cap()}</strong></td>
+                                        <tr key={keyid} className='hover:bg-gray-100 focus-within:bg-gray-100'>
+                                            <td><span className='px-4 py-6'>{propertyKey.cap()}</span></td>
                                             <td>{
-                                                permissionGroupByCategory[propertyKey].map((permission, keyId) => {
-                                                    return <span className='unit'>{permission.name}</span>;
+                                                groupedPermissions[propertyKey].map((permission, keyId) => {
+                                                    return <span key={keyId} className={'unit small check-'+(permissionCheck(permission)?'have':'not-have')}>{permission}</span>;
                                                 })
                                             }</td>
                                         </tr>
                                     )
                                 })}
+                                </tbody>
                             </table>
-                            
                         </div>
                     </div>
-                    
                 </div>
+                
             </Form>
         </MainLayout>
     );
