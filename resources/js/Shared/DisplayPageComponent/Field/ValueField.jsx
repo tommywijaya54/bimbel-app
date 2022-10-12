986f7391a7b8fba029ebe9b358b24d533c806398
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-12 16:42:08 +1100

    Update ValueField.jsx

--- resources/js/Shared/DisplayPageComponent/Field/ValueField.jsx
+++ resources/js/Shared/DisplayPageComponent/Field/ValueField.jsx
@@ -1,8 +1,18 @@
 import { FieldUtil } from '@/Shared/DisplayPageComponent/Field/util_field';
 
-export default ({field,data}) => {
-    if(!field.value){
-        field.value = data[field.entityname];
+export default ({field}) => {
+    const {entityname,value} = field;
+
+    if(entityname.includes('date')){
+        return <div className='no-wrap'>{(new Date(value)).toLocaleDateString(locale.code,locale.dateFormat)}</div>;
     }
-    return FieldUtil.getProcessedContent(field);
+
+    if(entityname.includes('amount') || entityname.includes('cost')){
+        return <span className='currency'>
+            <span className='sign'>{locale.currency.sign || '$'}</span>
+            {value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
+        </span>
+    }
+
+    return value; 
 }
