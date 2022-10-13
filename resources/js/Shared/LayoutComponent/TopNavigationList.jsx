8c2c32d715324a90f0f5369cec5f2d2b5374b09b
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-13 13:18:57 +1100

    Update Link to Dashboard

--- resources/js/Shared/LayoutComponent/TopNavigationList.jsx
+++ resources/js/Shared/LayoutComponent/TopNavigationList.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 import { Link, usePage } from '@inertiajs/inertia-react';
 
 export default function TopNavigationList() {
-    const permissions = usePage().props.auth.permissions;
-    
     const excludeFromTopNavList = ['user', 'branch', 'role', 'employee']; // for admin
-
-    let navlist = permissions.filter((alist) => {
+    let navlist = usePage().props.auth.permissions.filter((alist) => {
         return alist.includes('list-') && (!excludeFromTopNavList.includes(alist.split('-')[1]));
     });
 
     const NavList = ({list}) => {
         return <>
-        <Link
+        <a
             href='/chat'
-            className = 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+            className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
             >
             Chat
-        </Link>
+        </a>
         {
             list.map(((listonly,i) => {
                 const model = listonly.split("list-")[1];
