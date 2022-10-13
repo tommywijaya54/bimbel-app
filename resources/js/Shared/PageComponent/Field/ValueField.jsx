3e4ab55ce47efa9f7590cff356ad6dde62406140
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-13 11:09:22 +1100

    adding note field

--- resources/js/Shared/PageComponent/Field/ValueField.jsx
+++ resources/js/Shared/PageComponent/Field/ValueField.jsx
@@ -7,12 +7,18 @@ export default ({field}) => {
         return <div className='no-wrap'>{(new Date(value)).toLocaleDateString(locale.code,locale.dateFormat)}</div>;
     }
 
+    if(entityname.includes('note')){
+        return <div className='inline-note'>{value}</div>;
+    }
+
     if(entityname.includes('amount') || entityname.includes('cost')){
         return <span className='currency'>
             <span className='sign'>{locale.currency.sign || '$'}</span>
             {value.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')}
         </span>
     }
+
+
 
     return value; 
 }
