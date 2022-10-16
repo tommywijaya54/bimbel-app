c65f49111d346de3592ca54478da2ab4c1cedfaa
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-16 17:16:43 +1100

    Using useRef

--- resources/js/Shared/DataListInput.jsx
+++ resources/js/Shared/DataListInput.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default ({ label, name, className, errors = [], options, ...props }) => {
+export default ({ label, name, className, errors = [], options, Field, ...props }) => {
   const handleFocus = (event) => event.target.select();
+  const inputFieldRef = React.createRef();
+
+  const [valueid, setValueid] = useState(Field.model_value.id);
+  
+  const dataListRef = React.createRef();
+  const handleOnInput = (e) => {
+    const val = e.target.value;
+    const opts = dataListRef.current.childNodes;
+
+    for (var i = 0; i < opts.length; i++) {
+      if (opts[i].value === val) {
+        setValueid(opts[i].dataset.valueid);
+        break;
+      }
+    }
+  }
 
   return (
     <div className={className}>
@@ -10,17 +26,25 @@ export default ({ label, name, className, errors = [], options, ...props }) => {
           {label}:
         </label>
       )}
+
       <input
         list={name+'_list'}
+        onInput={handleOnInput}
+        
+        ref={inputFieldRef}
+        
         id={name}
         name={name}
         {...props}
+
         onFocus={handleFocus}
         className={`input-field form-input ${errors.length ? 'error' : ''}`}
+
+        data-valueid={valueid}
       />
-        <datalist id={name+'_list'}>
-            {options.map((value, keyId) => {
-              return  <option key={keyId} value={value.id+" : "+value.name}>{value.id+" : "+value.name}</option>
+        <datalist id={name+'_list'} ref={dataListRef}>
+            {options.map((option, keyId) => {
+              return  <option key={keyId} data-valueid={option.id} value={option.name}></option>
             })}
         </datalist>
     
