6c2b702a630adf66fa21ab1d60591fd6948bc864
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-16 13:19:22 +1100

    Salary component

--- resources/js/Pages/Employee/Show.jsx
+++ resources/js/Pages/Employee/Show.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import MainLayout from '@/Layouts/MainLayout';
 import Form from '@/Shared/PageComponent/Form/Form';
+import DetailsSummaryComponent from '@/Shared/PageComponent/Element/DetailsSummaryComponent';
+import { TableComponent, TableWithInlineForm } from '@/Shared/PageComponent/Table/TableComponent';
 
 export default function Show(props) {
     return (
         <MainLayout
             {...props}
         >
-            <h1 className='attention'>Employeee / Show</h1>
             <Form
                 {...props.form_schema}
             >
@@ -16,6 +17,23 @@ export default function Show(props) {
                     <div className="form-input">{props.roles.map((role,keyid) => <span key={keyid} className='unit'>{role}</span>)}</div>
                 </div>
             </Form>
+
+            <fieldset className='shadow-lg'>
+                <legend>Extra Information</legend>
+                <div className='p-6'>
+                    <DetailsSummaryComponent
+                            header='Salaries information'>
+                                <TableWithInlineForm
+                                    column='start_date:Start date,amount,note'
+                                    data={props.salaries}
+                                    
+                                    create_url={props.form_schema.id+'/salary'}
+                                    delete_url={props.form_schema.id+'/salary'}
+                                >
+                                </TableWithInlineForm>
+                    </DetailsSummaryComponent>
+                </div>
+            </fieldset>
         </MainLayout>
     );
 }
