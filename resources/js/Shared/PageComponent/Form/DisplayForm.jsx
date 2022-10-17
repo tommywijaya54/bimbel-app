b70eadea7e67f0965260ce4d2ce201cba0028b69
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-17 17:18:50 +1100

    Using ValueField.jsx then using the util function

--- resources/js/Shared/PageComponent/Form/DisplayForm.jsx
+++ resources/js/Shared/PageComponent/Form/DisplayForm.jsx
@@ -1,12 +1,8 @@
 import { FieldUtil } from "../../Util/Field_util";
+import ValueField from "../Field/ValueField";
+
 export default ({fields}) => {
     return fields.map((field,keyId) => {
-        return FieldUtil.getElementIfExist(field,keyId) || 
-                (<div key={keyId} className="form-component pr-6 pb-4 w-full lg:w-1/2">
-                    <label className="form-label">{field.label}:</label> 
-                    <div className="form-input">
-                        {FieldUtil.getProcessedContent(field,keyId)}
-                    </div>
-                </div>);
+        return <ValueField field={field} key={keyId}></ValueField>
     })
 }
