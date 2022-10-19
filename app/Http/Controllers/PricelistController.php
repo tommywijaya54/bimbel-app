1cc446834fcbf1e59cd616244a4363dfefd17355
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-19 17:26:14 +1100

    Update UI

--- app/Http/Controllers/PricelistController.php
+++ app/Http/Controllers/PricelistController.php
@@ -8,11 +8,23 @@ class PricelistController extends CommonController
     {
         parent::__construct([
             'list' => 'id:ID, start_date, end_date, branch_id, level,price,school_type,subject,week',
-            'form' => 'branch_id,level,price,school_type,week,subject,start_date,end_date,',
+            'form' => '
+                branch_id,
+                _,
+                school_type,
+                level,
+                subject,
+                week,
+                price,
+                _,
+                start_date,
+                end_date,',
         ], true);
 
         $this->form->title_format = "{subject}";
         $this->form->field('level')->hasOptions(['TKA', 'TKB', 'Grade 1', 'Grade 2', 'Grade 3', 'Grade 4', 'Grade 5', 'Grade 6', 'Grade 7', 'Grade 8', 'Grade 9', 'Grade 10', 'Grade 11', 'Grade 12']);
         $this->form->field('school_type')->hasOptions(['International', 'National', 'International & National']);
+        $this->form->field('week')->inputtype = 'number';
+        $this->form->field('price')->inputtype = 'currency';
     }
 }
