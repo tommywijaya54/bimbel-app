816f811aa5ec85b035359c3e52b35f4fd9b6c585
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-10-31 16:22:43 +1100

    Update Controller: Adding Sync Schedule and  Related Record

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -8,20 +8,20 @@ use App\Models\Schedule;
 use App\Models\ScheduleItem;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Inertia\Inertia;
 
 class ScheduleController extends CommonController
 {
-    //
     public function __construct()
     {
         parent::__construct([
             'list' => 'id:ID,class_subject,class_room,teacher_id',
-            'form' => 'teacher_id:Teacher Name,class_subject,branch_id,class_room,week|nr,students:Student Names'
+            'form' => 'teacher_id:Teacher Name,class_subject,branch_id,class_room,students_id:Student Names|ex'
         ], true);
 
-        // $this->list->item_url = 'schedule/{id}/details';
         $this->form->title_format = '{class_subject} / {class_room}';
+        array_push($this->form->with_list, 'items');
 
         $teacher_field = $this->form->field('teacher_id');
         $teacher_field->hasOptions(
@@ -30,50 +30,146 @@ class ScheduleController extends CommonController
         );
         $teacher_field->route['show'] = 'user.show';
 
-        $students_field = $this->form->field('students');
+        $students_field = $this->form->field('students_id');
+        $students_field->getValue = function ($field, $id, $data) {
+            return collect($data['students'])->map(function ($item) {
+                return $item['student_id'];
+            })->toArray();
+        };
         $students_field->hasOptions(
             User::role('Student')->get(['id', 'name'])->toArray(),
             'datalist-multiple-value'
         );
+    }
 
-        $this->form->item_form = new FormSchema('day:Hari|ex,session_date:Tanggal,session_start_time:Mulai jam,session_end_time:Selesai jam', 'Schedule Item', ScheduleItem::class);
-
-        $this->form->item_form->getField(
-            ['day'],
-            function ($field) {
-                $field->inputtype = 'none';
-                $field->required = false;
-                $field->attr = [
-                    'style' => ['display' =>  'none']
-                ];
-            }
-        );
-
-        $this->form->item_form->getField(
-            ['session_start_time', 'session_end_time'],
-            function ($field) {
-                $field->inputtype = 'time';
-            }
-        );
+    public function create()
+    {
+        $form = $this->form->createForm();
+        return Inertia::render('Schedule/Details', [
+            'title' => 'Schedule Details',
+            'form_schema' => $form,
+            'schedule' => $form->data,
+        ]);
     }
 
     public function show($id)
     {
-        $schedule = $this->entity::with('items')->find($id);
         $form = $this->form->editForm($id);
-
         return Inertia::render('Schedule/Details', [
             'title' => 'Schedule Details',
             'form_schema' => $form,
-            'schedule' => $schedule,
+            'schedule' => $form->data,
         ]);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            "teacher_id" => "required",
+            "class_subject" => "required",
+            "branch_id" => "required",
+            "class_room" => "required",
+            "students_id" => "required",
+            "items" => "required",
+        ]);
+
+        $schedule = Schedule::create([
+            "teacher_id" => $request->teacher_id,
+            "class_subject" => $request->class_subject,
+            "branch_id" => $request->branch_id,
+            "class_room" => $request->class_room
+        ]);
+
+        $students = collect($request->students_id)->map(function ($item) {
+            return ['student_id' => $item];
+        })->toArray();
+        $schedule->students()->createMany($students);
+        $schedule->items()->createMany($request->items);
+
+        return redirect('/' . $this->modal . '/' . $schedule->id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            "teacher_id" => "required",
+            "class_subject" => "required",
+            "branch_id" => "required",
+            "class_room" => "required",
+            "students_id" => "required",
+            "items" => "required",
+        ]);
+
+        $schedule = Schedule::with(['students', 'items'])->find($id);
+        $schedule->update([
+            "teacher_id" => $request->teacher_id,
+            "class_subject" => $request->class_subject,
+            "branch_id" => $request->branch_id,
+            "class_room" => $request->class_room
+        ]);
+
+        // 
+
+        // Update Student (Delete & Create)
+        $students_id = $schedule->students->pluck('student_id');
+        $delete_students = $students_id->diff($request->students_id);
+        $schedule->students->filter(function ($item) use ($delete_students) {
+            return $delete_students->first(function ($itemid, $key) use ($item) {
+                return $itemid == $item->student_id;
+            });
+        })->each(function ($item) {
+            $item->forceDelete();
+        });
+        $schedule->students()->createMany((collect($request->students_id)->diff($students_id))->map(function ($item) {
+            return ['student_id' => $item];
+        }));
+
+        // Update Items (Delete)
+        $items_dates = $schedule->items->pluck('session_date');
+        dd($request->items);
+
+        $request_items = collect($request->items);
+        $delete_items = '';
+        $new_items = '';
+
+        echo '<pre>';
+
+        if ($items_dates[0] == $request_items[0]) {
+            echo '\n= = = same date = = =\n';
+        } else {
+            echo "\n not same \n";
+        }
+
+        echo '<strong>items_dates</strong><br>';
+        print_r($items_dates);
+
+
+        echo '<strong>request_items</strong><br>';
+        print_r($request_items);
+
+        echo '</pre>';
+        dd($items_dates);
+
+        // diff -> create new item
+    }
+
+    public function delete_schedule($id)
+    {
+        $schedule = Schedule::with(['students', 'items'])->find($id);
+        $schedule->students->each(function ($item) {
+            $item->forceDelete();
+        });
+        $schedule->items->each(function ($item) {
+            $item->forceDelete();
+        });
+        $schedule->forceDelete();
+        return redirect('/' . $this->modal);
+    }
+
 
     public function add_item($id, Request $request)
     {
         $schedule = Schedule::find($id);
-        //dd($request['session_date']);
-
         $schedule->items()->create([
             'session_date' => $request['session_date'],
             'session_start_time' => $request->session_start_time,
