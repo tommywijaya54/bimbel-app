ce84ca86b02613f457e6fde3f6a1afdb256f76cc
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-11-01 18:23:34 +1100

    Adding timetable link

--- resources/js/Shared/LayoutComponent/TopNavigationList.jsx
+++ resources/js/Shared/LayoutComponent/TopNavigationList.jsx
@@ -15,6 +15,12 @@ export default function TopNavigationList() {
             >
             Chat
         </a>
+        <a
+            href='/timetable'
+            className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+            >
+            Timetable
+        </a>
         {
             list.map(((listonly,i) => {
                 const model = listonly.split("list-")[1];
