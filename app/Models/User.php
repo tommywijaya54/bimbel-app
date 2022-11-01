199d24e34c02aa1de011430ca02db37660713fa0
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-11-01 18:22:56 +1100

    Adding schedule

--- app/Models/User.php
+++ app/Models/User.php
@@ -72,6 +72,14 @@ class User extends Authenticatable
         return static::where('name', $fullname)->first();
     }
 
+    public function schedules()
+    {
+        $ids = ScheduleStudent::where('student_id', $this->id)->pluck('schedule_id');
+        $schedules = Schedule::with(['items', 'branch', 'teacher'])->find($ids);
+        return $schedules;
+    }
+
+
     public function details()
     {
         $type = $this->type;
