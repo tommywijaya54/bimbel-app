1da18496ff9f1c1696e190f2c27276beb34d532e
tommywijaya54 <tommy.wijaya54@yahoo.com>
2022-11-02 13:31:14 +1100

    Update TopNavigationList.jsx

--- resources/js/Shared/LayoutComponent/TopNavigationList.jsx
+++ resources/js/Shared/LayoutComponent/TopNavigationList.jsx
@@ -7,20 +7,20 @@ export default function TopNavigationList() {
         return alist.includes('list-') && (!excludeFromTopNavList.includes(alist.split('-')[1]));
     });
 
+    const Nav = ({href,children}) => {
+        return <a
+            href={href}
+            className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
+            >
+            {children}
+        </a>
+    }
+
     const NavList = ({list}) => {
         return <>
-        <a
-            href='/chat'
-            className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-            >
-            Chat
-        </a>
-        <a
-            href='/timetable'
-            className='inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out'
-            >
-            Timetable
-        </a>
+        <Nav href='/chat'>Chat</Nav>
+        <Nav href='/timetable'>Timetable</Nav>
+        <Nav href='/attendance'>Attendance</Nav>
         {
             list.map(((listonly,i) => {
                 const model = listonly.split("list-")[1];
